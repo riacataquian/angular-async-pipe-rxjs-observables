2fbad3145854bf998524beb1dff433d411c575bd
riacataquian <icataquian@gmail.com>
2018-01-26 17:36:09 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListContainerComponent } from './list-container/list-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListContainerComponent
   ],
   imports: [
     BrowserModule
