2cf19acc1d9ccc0c1b5b0951b8ca6a2c31be895d
riacataquian <icataquian@gmail.com>
2018-02-05 17:40:15 +0800

    Fix failing tests

--- src/app/backend.service.spec.ts
+++ src/app/backend.service.spec.ts
@@ -1,15 +1,24 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { BackendService } from './backend.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 describe('BackendService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BackendService]
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        BackendService,
+      ]
     });
   });
 
-  it('should be created', inject([BackendService], (service: BackendService) => {
+  it('should be created', inject([BackendService, HttpClientModule], (service: BackendService) => {
     expect(service).toBeTruthy();
   }));
 });
--- src/app/feed-card/feed-card.component.spec.ts
+++ src/app/feed-card/feed-card.component.spec.ts
@@ -20,6 +20,6 @@ describe('FeedCardComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+   expect(component).toBeTruthy();
   });
 });
